02c9cbe9eb577d71562c46653eb95089b45769e2
Jozef <jozefw@sbcglobal.net>
2014-05-03 12:46:21 -0700

    First Commit on Swear Jar

---
+++ Swear_jar/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwearJar::Application.config.secret_key_base = '08aa2085d7910bb1672e4a693b5b44163164a394c08835936971935d85d35a9a11888020cb4823367923d51a790277a14302c8c3a372240a5222e2529ac8c630'
---
+++ Swear_jar/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwearJar::Application.config.session_store :cookie_store, key: '_Swear_jar_session'
